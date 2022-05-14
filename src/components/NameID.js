195af1de03b82acf4d6e84119ddd1f960631d6b1
Ahmed Mohamed AbdElMoneam <ahmedabdelmoneam755@gmail.com>
2022-05-14 22:41:09 +0200

    V3

--- src/components/NameID.js
+++ src/components/NameID.js
@@ -2,14 +2,16 @@ import '../membership-styling.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
 
-const NameID = (props) => {
-    return (
+const NameID = ({ userData }) => {
+    return ( 
         <div>
             <div id='membership-container'>
                 <ul id='membership-list'>
                     <li id="user-data">
-                        <p className='mem-userNameID' style={{ fontSize: "2em" }}>{props.name}</p>
-                        <p className='mem-userNameID'>User ID: {props.id}</p>
+                        <p className='mem-userNameID' style={{fontSize: "2em"}}>
+                            {`${userData.firstName} ${userData.lastName}`}
+                        </p>
+                        <p className='mem-userNameID'>User ID: {`${userData.id}`}</p>
                     </li>
                     <li>
                         <FontAwesomeIcon icon={solid('circle-user')} size="6x" />
--- src/components/SubscriptionTable.js
+++ src/components/SubscriptionTable.js
@@ -1,30 +1,28 @@
 import '../membership-styling.css';
 
-const getDays = (date) => {
-    const date1 = new Date(date);
-    const date2 = new Date();
-    const diffInTime = date1.getTime() - date2.getTime();
-    var Difference_In_Days = diffInTime / (1000 * 3600 * 24);
-    return Difference_In_Days;
-}
-
-const SubscriptionTable = (props) => {
-    console.log(props)
-    return (
+const SubscriptionTable = ({ subscription }) => {
+    return ( 
         <div className='membership-tableContainer'>
             <table className="membership-table">
-                <tr>
-                    <th>Subscription Status</th>
-                    <td>{props.status}</td>
-                </tr>
-                <tr>
-                    <th>Subscription Date</th>
-                    <td>{props.date}</td>
-                </tr>
-                <tr>
-                    <th>Subscription End Date</th>
-                    <td>{getDays(props.date) > 0 ? Math.floor(getDays(props.date)) : '--'}</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th>Subscription Status</th>
+                        {subscription.active === "true" ? 
+                            <td><span className='activation-expiration' style={{backgroundColor: "green"}}>
+                                Active</span></td> : 
+                            <td><span className='activation-expiration' style={{backgroundColor: "red"}}>
+                                Expired</span></td>
+                        }
+                    </tr>
+                    <tr>
+                        <th>Subscription Date</th>
+                        <td>{subscription.subDate}</td>
+                    </tr>
+                    <tr>
+                        <th>Subscription End Date</th>
+                        <td>{subscription.endDate} Days left</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
